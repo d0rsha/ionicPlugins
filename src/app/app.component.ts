a00a1abd05e8db45c6023146d51bcf73c20a63f2
d0rshBooken <andvr916@student.liu.se>
2019-05-02 14:20:59 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,30 +27,9 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
 
-      console.log('Platform.ready(): ', readySource)
-      console.log(window)
-      // App is initialized here !
-      this.fpm = window['FirebasePlugin']
-
-
-      // Only enable in production environent 
-      // envrionment variable is set to normal or prod during compile-time 
-      if (environment.production) {
-        console.log('--prod mode')
-        this.fpm.setPerformanceCollectionEnabled(true)
-        this.fpm.setAnalyticsCollectionEnabled(true)
-  
-        /*this.fpm.startTrace('platform_ready_to_first_page', success => {
-          console.log(success)
-        }, error => {
-          console.log(error)
-        })*/  
-      } else {
-        console.log('normal mode')
-      }
-
-
-      this.makeHttpRequest()
+      window["ReportFullyDrawn"].reportFullyDrawn();
+      window["ReportFullyDrawn"].printInfo();
+      // this.makeHttpRequest()
     });
   }
 
